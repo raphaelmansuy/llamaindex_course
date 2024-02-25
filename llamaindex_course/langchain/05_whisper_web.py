12bf6e3ded819405eb49dc195607315a18926ef9
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-02-25 21:50:31 +0800

    update whisper

--- llamaindex_course/langchain/05_whisper_web.py
+++ llamaindex_course/langchain/05_whisper_web.py
@@ -2,14 +2,15 @@ import gradio as gr
 from transformers import pipeline
 import numpy as np
 
-transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
+transcriber = pipeline("automatic-speech-recognition",
+                       model="openai/whisper-base.en")
+
 
 def transcribe(audio):
-   """ Transcribe the audio using the OpenAI Whisper model. """
+    """ Transcribe the audio using the OpenAI Whisper model. """
     sr, y = audio
     y = y.astype(np.float32)
     y /= np.max(np.abs(y))
-
     return transcriber({"sampling_rate": sr, "raw": y})["text"]
 
 
