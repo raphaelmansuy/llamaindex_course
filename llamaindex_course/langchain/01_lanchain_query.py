dcb60e00701702e191a7c44e56f56b24e4e9bd9c
Raphael MANSUY <raphael.mansuy.partner@decathlon.com>
2024-03-02 18:38:54 +0800

    update

--- llamaindex_course/langchain/01_lanchain_query.py
+++ llamaindex_course/langchain/01_lanchain_query.py
@@ -1,13 +1,17 @@
-
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms import Ollama
 
 
+LLM_MODEL = "mistral:latest"
 
+# pylint: disable=E1102
+model = Ollama(model=LLM_MODEL)
 
 
 prompt = ChatPromptTemplate.from_template(
-    "tell me a joke about {foo}, more than {word_count} words")
+    "tell me a joke about {foo}, more than {word_count} words"
+)
 chain = prompt | model | StrOutputParser()
 
 for chunk in chain.stream({"foo": "bears", "word_count": 200}):
